09bb419bef8f66a410f58b05b7ec613abc349a46
Sydney Young <sydneyyoung17@gmail.com>
2021-01-02 17:37:19 -0800

    Create, run, and delete container
    Docker API learning/testing. Create, runs, and deletes container. Does not check for existing containers.

---
+++ database_migration_checker.py
@@ -0,0 +1,22 @@
+import docker
+
+# Test if containers are already running or not
+# Handle running containers gracefully by clean starts
+
+def main():
+    # Initialize Docker
+    client = docker.from_env()
+
+    # Start containers
+    old_db = client.containers.create(image="guaranteedrate/homework-pre-migration:1607545060-a7085621", publish_all_ports=True)
+    old_db.start()
+
+    # Show containers
+    print(client.containers.list())
+    old_db.stop()
+    client.containers.prune()
+
+    pass
+
+if __name__ == "__main__":
+    main()
